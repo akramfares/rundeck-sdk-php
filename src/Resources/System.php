fecc98a5d518579508f4a9f1d0079fcea88ab7a2
Akram Fares <akramfares@gmail.com>
2017-05-06 14:55:26 +0100

    Add Unit Tests for \Rundeck\Resources\System

--- src/Resources/System.php
+++ src/Resources/System.php
@@ -4,23 +4,48 @@ namespace Rundeck\Resources;
 
 use Rundeck\HttpClient;
 
+/**
+ * Class System /api/V/system/
+ * @package Rundeck\Resources
+ */
 class System
 {
+    /**
+     * @var null
+     */
     private $name;
+
+    /**
+     * @var HttpClient
+     */
     private $client;
 
+    /**
+     * @var array
+     */
     private $actions = [
-        "info",
-        "logstorage",
-        "logstorage/incomplete"
+        "info", // /api/V/system/info
+        "logstorage", // /api/V/system/logstorage
+        "logstorage/incomplete" // /api/V/system/logstorage/incomplete
     ];
 
+    /**
+     * @param HttpClient $client
+     * @param null $name
+     */
     public function __construct(HttpClient $client, $name = null)
     {
         $this->name = $name;
         $this->client = $client;
     }
 
+    /**
+     * Get System action
+     * @param $action
+     * @param string $alt xml|json
+     * @return array
+     * @throws \Exception
+     */
     public function get($action, $alt = "xml")
     {
         if (in_array($action, $this->actions)) {
---
+++ tests/SystemTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Rundeck;
+
+use Rundeck\Resources\System;
+
+class SystemTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test get system info
+     */
+    public function testGetInfo()
+    {
+        $httpClient = new MockHttpClient(__DIR__."/data/system/info.xml");
+
+        $system = new System($httpClient->getClient());
+
+        $this->assertArrayHasKey("rundeck", $system->get("info"));
+    }
+
+    /**
+     * Test get log storage
+     */
+    public function testGetLogstorage()
+    {
+        $httpClient = new MockHttpClient(__DIR__."/data/system/logstorage.xml");
+
+        $system = new System($httpClient->getClient());
+
+        $this->assertArrayHasKey("succeededCount", $system->get("logstorage"));
+    }
+
+    /**
+     * Test get log storage incomplete
+     */
+    public function testGetLogstorageIncomplete()
+    {
+        $httpClient = new MockHttpClient(__DIR__."/data/system/logstorage-incomplete.xml");
+
+        $system = new System($httpClient->getClient());
+
+        $this->assertArrayHasKey("execution", $system->get("logstorage/incomplete"));
+    }
+}
