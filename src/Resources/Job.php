a63df8667b169a048e14ef973dffa6595aec4474
Akram Fares <akramfares@gmail.com>
2017-05-07 17:17:29 +0100

    Add Unit Tests for \Rundeck\Resources\Job

--- src/Resources/Job.php
+++ src/Resources/Job.php
@@ -4,30 +4,59 @@ namespace Rundeck\Resources;
 
 use Rundeck\HttpClient;
 
+/**
+ * Class Job
+ * @package Rundeck\Resources
+ */
 class Job
 {
 
+    /**
+     * @var string
+     */
     private $name;
+
+    /**
+     * @var HttpClient
+     */
     private $client;
 
+    /**
+     * @var array
+     */
     private $actions = [
         "executions" => ["xml"],
         "input/files" => ["xml"],
         "info" => ["xml"],
     ];
 
+    /**
+     * @param HttpClient $client
+     * @param string $name
+     */
     public function __construct(HttpClient $client, $name = null)
     {
         $this->name = $name;
         $this->client = $client;
     }
 
+    /**
+     * @param string $alt
+     * @return mixed
+     */
     public function find($alt = "xml")
     {
         $response = $this->client->get('/job/'.$this->name, $alt);
         return $response;
     }
 
+    /**
+     * Get Job action
+     * @param $action
+     * @param string $alt xml|json
+     * @return array
+     * @throws \Exception
+     */
     public function get($action, $alt = "xml")
     {
         if (array_key_exists($action, $this->actions)) {
---
+++ tests/JobTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Rundeck;
+
+use Rundeck\Resources\Job;
+
+class JobTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test get job info
+     */
+    public function testGetInfo()
+    {
+        $httpClient = new MockHttpClient(__DIR__."/data/job/find.xml");
+
+        $job = new Job($httpClient->getClient(), "c4ec2b60-ac83-4ee2-9266-67ce795c9603");
+
+        $this->assertArrayHasKey("job", $job->find());
+    }
+
+    /**
+     * Test get job executions
+     */
+    public function testGetHistory()
+    {
+        $httpClient = new MockHttpClient(__DIR__."/data/job/executions.xml");
+
+        $job = new Job($httpClient->getClient(), "c4ec2b60-ac83-4ee2-9266-67ce795c9603");
+
+        $this->assertArrayHasKey("execution", $job->get("executions"));
+    }
+}
